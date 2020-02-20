d12a5125517780c5250acf13ed359f722351c8e1
Marcus Felix <marcus@samhaus.com>
2020-02-20 06:01:43 -0800

    nova home

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -10,27 +10,29 @@ export default {
           "Unimed",
           "Amil",
           "IPASGO",
+          "Particular",
+          "Outros"
         ],
         datasets: [
           {
             label: "Exames",
             backgroundColor: "#00c853",
-            data: [15, 8, 5]
+            data: [15, 8, 5, 12, 5]
           },
           {
             label: "Retornos",
             backgroundColor: "#17a2b8",
-            data: [3, 3, 3]
+            data: [3, 3, 3, 5, 2]
           },
           {
             label: "Consultas",
             backgroundColor: "#20c997",
-            data: [90, 44, 13]
+            data: [90, 44, 13, 30, 20]
           },
           {
             label: "Total",
             backgroundColor: "#6610f2",
-            data: [40, 39, 10]
+            data: [40, 39, 10, 30, 15]
           },
           
         ]
--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -4,7 +4,7 @@
       <div class="row">
         <div class="col-lg-3">
           <div class="block text green">
-            <h5>Pacientes</h5>
+            <h5>Agendamentos</h5>
             <h3>Agendados</h3>
             <div class="number">
               <big>45</big>
@@ -13,8 +13,8 @@
         </div>
         <div class="col-lg-3">
           <div class="block text purple">
-            <h5>Pacientes</h5>
-            <h3>Confirmados</h3>
+            <h5>Agendamentos</h5>
+            <h3>Transferidos</h3>
             <div class="number">
               <big>40</big>
             </div>
@@ -22,8 +22,8 @@
         </div>
         <div class="col-lg-3">
           <div class="block text blue">
-            <h5>Pacientes</h5>
-            <h3>Atendidos</h3>
+            <h5>Agendamentos</h5>
+            <h3>Encaixes</h3>
             <div class="number">
               <big>15</big>
             </div>
@@ -31,8 +31,8 @@
         </div>
         <div class="col-lg-3">
           <div class="block text red">
-            <h5>Pacientes</h5>
-            <h3>Faltantes</h3>
+            <h5>Agendamentos</h5>
+            <h3>Retornos</h3>
             <div class="number">
               <big>3</big>
             </div>
@@ -63,7 +63,7 @@
               </div>
               <h3>Convênios</h3>
             </div>
-            <table class="table table-plans table-borderless" style="height: 375px" v-if="!graph">
+            <table class="table table-plans table-borderless" style="height: 280px" v-if="!graph">
               <thead>
                 <tr>
                   <th scope="col">Convênio</th>
@@ -95,50 +95,58 @@
                   <td class="text-center">5</td>
                   <td class="text-center">23</td>
                 </tr>
+                <tr>
+                  <th scope="row"> Particular</th>
+                  <td class="text-center">13</td>
+                  <td class="text-center">3</td>
+                  <td class="text-center">5</td>
+                  <td class="text-center">23</td>
+                </tr>
+                <tr>
+                  <th scope="row"> Outros</th>
+                  <td class="text-center">13</td>
+                  <td class="text-center">3</td>
+                  <td class="text-center">5</td>
+                  <td class="text-center">23</td>
+                </tr>
               </tbody>
             </table>
             <div style="padding-top: 15px" v-if="graph">
-              <BarChart :height="390"/>
+              <BarChart :height="280"/>
             </div>
           </div>
         </div>
       </div>
-      <!--<div class="row">
-        <div class="col-lg-12">
-          <div class="block">
-            <div>
-              <h3>Agendamentos para hoje</h3>
-            </div>
-            <table class="table table-striped table-hover">
-              <thead>
-                <tr>
-                  <th scope="col" width="45px">Paciente</th>
-                  <th scope="col" width="50%"></th>
-                  <th scope="col">Médico</th>
-                  <th scope="col">Hora</th>
-                  <th scope="col">Status</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="(item, index) in list" :key="index">
-                  <th scope="row">
-                    <div class="avatar" :style="'background-image: url(' + item.photo + ');'"></div>
-                  </th>
-                  <td>{{item.name}}</td>
-                  <td>{{item.doctor}}</td>
-                  <td>{{item.time}}</td>
-                  <td>
-                    <span class="badge" v-bind:class="item.status == 'Confirmado' ? 'badge-primary' : item.status == 'Faltante' ? 'badge-danger' : 'badge-secondary'">{{item.status.toUpperCase()}}</span>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>-->
       <div class="row">
-        <br/>
+        <div class="col-lg-4">
+          <div class="block text transparent">
+            <h5>Pacientes</h5>
+            <h3>Atendidos</h3>
+            <div class="number">
+              <big>45</big>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="block text transparent">
+            <h5>Pacientes</h5>
+            <h3>Cancelados</h3>
+            <div class="number">
+              <big>40</big>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="block text transparent">
+            <h5>Pacientes</h5>
+            <h3>Bloqueados</h3>
+            <div class="number">
+              <big>15</big>
+            </div>
+          </div>
+        </div>
       </div>
+      <div class="row"></div>
     </div>
   </div>
 </template>
@@ -159,29 +167,6 @@ export default {
   },
   data: () => ({
     graph: false,
-    list: [
-      {
-        "photo": "https://www.ahbvlp.pt/wp-content/uploads/2018/07/avatar.jpg",
-        "name": "Alex Murphy",
-        "doctor": "Badan Palhares",
-        "time": "14:15",
-        "status": "Confirmado",
-      },
-      {
-        "photo": "https://www.ahbvlp.pt/wp-content/uploads/2018/07/avatar.jpg",
-        "name": "Genghis Khan",
-        "doctor": "Badan Palhares",
-        "time": "12:00",
-        "status": "Pendente",
-      },
-      {
-        "photo": "https://www.ahbvlp.pt/wp-content/uploads/2018/07/avatar.jpg",
-        "name": "Tutankhamon",
-        "doctor": "Badan Palhares",
-        "time": "08:00",
-        "status": "Faltante",
-      },
-    ],
     events: [
       {
         start: '2018-11-21',
@@ -243,7 +228,7 @@ export default {
     padding: 30px 20px 5px 30px
   }
   .block big {
-    font-size: 3.5em;
+    font-size: 3em;
     font-weight: 900;
     margin-right: 15px;
     color: #0000009e;
@@ -261,7 +246,7 @@ export default {
     margin-bottom: 30px
   }
   .cal {
-    height: 492px;
+    height: 414px;
     padding: 15px
   }
   .table-plans {
@@ -282,6 +267,9 @@ export default {
   }
   .green {
     background-color: #00c8532e
+  }
+  .transparent {
+    background-color: transparent
   }
   .switch {
     float: right;
